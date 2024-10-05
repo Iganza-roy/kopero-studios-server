abfdf9214cabd990045e085d16756f33bbda7df8
Peter Omollo <omollpeter@gmail.com>
2024-10-05 11:51:54 +0300

    Create kopero django project     - Add 4 apps: kopero_auth, booking, services, payments     - Configure the project's settings module

---
+++ kopero/settings/development.py
@@ -0,0 +1,23 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Database credentials
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'HOST': config("DB_HOST"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'PORT': config("DB_PORT"),
+    }
+}
