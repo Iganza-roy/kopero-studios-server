abb5ef6e4c719260785116dca192c34d0c2b8a41
Peter Omollo <omollpeter@gmail.com>
2024-10-10 10:30:36 +0300

    Create a view for the service endpoint

--- services/views.py
+++ services/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ServiceSerializer
+from .models import Service
 
 # Create your views here.
+class ServiceViewSet(ModelViewSet):
+    """
+    Class based viewset for the service endpoint
+    """
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
