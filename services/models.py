22a5b54a9da8dbe97eae54555bcf92297cd8194b
Peter Omollo <omollpeter@gmail.com>
2024-10-23 06:12:21 +0300

    Update service model

--- services/models.py
+++ services/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
 class Service(models.Model):
     """
     Represents services database table
     """
-    name = models.CharField(max_length=200, null=False)
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    name = models.CharField(max_length=200, null=False, unique=True)
     tag = models.CharField(max_length=200, null=False)
     description = models.TextField(max_length=2000, blank=True)
     rate_per_hour = models.FloatField(default=1000.00)
