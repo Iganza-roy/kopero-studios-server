8e6bf3d763fdd2f3ca2dec525168881d8953333f
Peter Omollo <omollpeter@gmail.com>
2024-10-28 06:48:18 +0300

    Configure email backend to use smtp

--- kopero/settings/base.py
+++ kopero/settings/base.py
@@ -188,7 +188,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = "Kopero Studios <koperostudios@gmail.com>"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = config("EMAIL_PORT", cast=int)
